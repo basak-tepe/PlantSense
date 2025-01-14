f11e2f4d7474e0108d47b9e34107de10e9d3c9b2
Basak Tepe <basaktepe2020@gmail.com>
2025-01-14 14:28:26 +0300

    Implemented soil humidity measurement

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,27 @@
 #include <Arduino.h>
 
-#define OUTPUT_PIN A5
+#define OUTPUT_PIN A5            
+#define SOIL_SENSOR_ANALOG_PIN A0 
 
 void setup() {
-  pinMode(OUTPUT_PIN, OUTPUT);
+  pinMode(OUTPUT_PIN, OUTPUT); 
+  Serial.begin(9600);          // Initialize serial communication at 9600 baud
 }
 
 void loop() {
-  digitalWrite(OUTPUT_PIN, HIGH);
-  delay(1000); 
+  int analogValue = analogRead(SOIL_SENSOR_ANALOG_PIN);
 
-  // Set the pin LOW
-  digitalWrite(OUTPUT_PIN, LOW);
-  delay(1000);
+  Serial.print("Analog Value: ");
+  Serial.println(analogValue);
+
+  // Control the output pin based on soil moisture level
+  if (analogValue > 500) { 
+    digitalWrite(OUTPUT_PIN, HIGH);
+    Serial.println("Soil is dry! Water is needed.");
+  } else {
+    digitalWrite(OUTPUT_PIN, LOW);
+    Serial.println("Soil is moist. No watering needed.");
+  }
+
+  delay(5000); // 5s
 }
